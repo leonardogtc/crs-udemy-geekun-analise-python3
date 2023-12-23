d1d78e78e4d6192d3b73e2c05d52f28ea8745a98
Leonardo Gonçalves Tavares da Conceição <leonardogtc@gmail.com>
2023-12-23 20:29:13 -0300

    Alteração na aula 07

--- dataviz/s03-matplotlib/07-main.py
+++ dataviz/s03-matplotlib/07-main.py
@@ -9,10 +9,10 @@ fig, ax = plt.subplots(2, 2)
 
 # Dados
 indices = [1, 2, 3, 4]
-amostra_a = [1, 4, 2, 3]
-amostra_b = [2, 8, 4, 6]
-amostra_c = [5, 12, 7, 9]
-amostra_d = [12, 8, 4, 6]
+sample_a = [1, 4, 2, 3]
+sample_b = [2, 8, 4, 6]
+sample_c = [5, 12, 7, 9]
+sample_d = [12, 8, 4, 6]
 
 ax[0][0].set_title('06:00 - 12:00')
 ax[0][1].set_title('12:00 - 18:00')
@@ -20,10 +20,10 @@ ax[1][0].set_title('18:00 - 00:00')
 ax[1][1].set_title('00:00 - 06:00')
 
 # plot data on axes
-ax[0][0].plot(indices, amostra_a, label="Amostra A", marker='o')
-ax[0][1].plot(indices, amostra_b, label="Amostra B", color='green', marker='o')
-ax[1][0].plot(indices, amostra_c, label="Amostra C", color='red', marker='o')
-ax[1][1].plot(indices, amostra_d, label="Amostra D", color='yellow', marker='o')
+ax[0][0].plot(indices, sample_a, label="sample A", marker='o')
+ax[0][1].plot(indices, sample_b, label="sample B", color='green', marker='o')
+ax[1][0].plot(indices, sample_c, label="sample C", color='red', marker='o')
+ax[1][1].plot(indices, sample_d, label="sample D", color='yellow', marker='o')
 
 ax[0][0].legend()
 ax[0][1].legend()
